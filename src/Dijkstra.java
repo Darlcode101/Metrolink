e22672065e1242de88a1f5b6535554d216f9bd91
alex <alexanderdarlingtonbusiness@gmail.com>
2026-04-28 17:16:05 +0100

    made the logic better for fewest changes

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -9,15 +9,15 @@
 
         private double totTime;
         private int changeCount = 0;
+        
 
-            public List<String> findRoute(Hashmap hashmap, String start, String end){
+            public List<String> findRoute(Hashmap hashmap, String start, String end, boolean useCost){
                 
                 
                 Map<String, Double> distances = new HashMap<>();
                 Map<String, String> previous = new HashMap<>();
                 Map<String, String> previousColour = new HashMap<>();
-                String firstcolour = "";
-
+               
                 Set<String> details = hashmap.getAllStations();
                     for (String station : details) {
                         distances.put(station, Double.MAX_VALUE);
@@ -40,32 +40,43 @@
                     }
                     
                     for (Edge edge : hashmap.getNeighbours(current)){
-                        if (current.equals(start) && firstcolour.equals("")) {
-                            firstcolour = edge.colour;
-                        }
-
+                        
+                       
                         String neighbour = edge.neighbour;
                         double travelTime = edge.time;
                         String compareColour = edge.colour;
-                        
-                        
-                        
-                        
-                        
-                        if(previousColour.get(current)!= null && !previousColour.get(current).equalsIgnoreCase(compareColour)){
-                            travelTime += 2;
+                        int penalty = 0;
+
+                        if(previousColour.get(current)!= null && !previousColour.get(current).equalsIgnoreCase(compareColour))
+                        {
+                            if (useCost){
+                                penalty = 100;
+                            }
+                            else{
+                                travelTime += 2;
+                            }
                         }
                         
                         
-                        
-                        totTime = distances.get(current) + travelTime ; 
+                        totTime = distances.get(current) + travelTime + penalty ; 
+
                         if (totTime < distances.get(edge.neighbour)){
                             
                             distances.put(edge.neighbour, totTime);
                             previous.put(neighbour, current);
                             previousColour.put(neighbour, edge.colour);
+
+                            if (current.equals(start)){
+                                previousColour.put(start, edge.colour);
+                            }
+
                             pq.add(neighbour);
+
+
                         }
+                        
+                        
+
                 }
             }
             List<String> route = new ArrayList<>();
@@ -76,11 +87,9 @@
                 String prevStation = previous.get(current);
                 String colour = previousColour.get(current);
 
-                if (colour != null){
+                
                     route.add(0, current + " on " + colour + " line");
-                }else {
-                route.add(0, current + " on " + firstcolour + " line");
-                }
+                
                 if (previousColour.get(prevStation)!= null&&(!previousColour.get(current).equalsIgnoreCase(previousColour.get(prevStation)))){
                     route.add(0,"*** Change to the " +previousColour.get(current)+" line ***");
                     changeCount ++;
@@ -88,6 +97,10 @@
                 current = previous.get(current);
             }   
             totTime = distances.get(end);
+            
+            if (useCost){
+                totTime = totTime - (changeCount  * 100) + (changeCount * 2);
+            }
             return route;
         }
         public double getTotTime() {
